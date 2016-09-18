64ff12b3505d394067fe4e646e681f55166a8e0c
youseatao <544425908@qq.com>
2016-09-18 09:02:15 -0700

    form

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 import views
 urlpatterns = [
 	url(r"^$", views.mainpage),
+	url(r"^submit$", views.submit),
+	url(r"^wait$", views.wait),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 def mainpage(request):
 	return render(request, "main/main.html")
+
+def submit(request):
+	if request.method == "POST":
+		info = request.POST
+		return HttpResponseRedirect("/wait", {"info":info})
+
+def wait(request):
+	return render(request, "main/wait.html")
